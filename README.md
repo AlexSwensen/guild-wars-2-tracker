c9812f54f5e056866ad717855da5049335671eac
Alexander Swensen <alex.swensen@gmail.com>
2015-11-24 17:42:12 -0500

    Initial commit of starting project. We will build it out from here. - Modified gulp workflow and project structure, we will continue to finish modifying it, but ultimately it will be much more scaleable over the long run.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# guild-wars-2-daily-tracker
+# Guild Wars 2 Daily Tracker
+A Guild Wars 2 daily achievement tracker for those with a busy schedule.
+
+--
+I built this app for the following reasons.
+
+- As a way to give back to the community I love so much.
+- As a medium to practice the skills i use every day at work in a medium i can destribute openly.
+- As a method of exploring the Guild Wars 2 API.
+- To make my life (and others, hopefully) a little more convenient.
+- To show off the awesome work that the guys over at the Ionic Framework Team have done.
---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var bower = require('bower');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var sh = require('shelljs');
+var inject = require('gulp-inject');
+
+var paths = {
+  sass: ['./scss/**/*.scss'],
+  img: ['./www/assets/img/*']
+};
+
+gulp.task('default', ['sass', 'inject']);
+
+gulp.task('sass', function(done) {
+  gulp.src('./scss/ionic.app.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./www/assets/css/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./www/assets/css/'))
+    .on('end', done);
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.sass, ['sass']);
+});
+
+gulp.task('install', ['git-check'], function() {
+  return bower.commands.install()
+    .on('log', function(data) {
+      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+    });
+});
+
+gulp.task('inject', function () {
+  var target = gulp.src('./www/index.html');
+  var sources = gulp.src('./www/app/**/*.js', {read: false});
+  return target.pipe(inject(sources, {relative: true}))
+    .pipe(gulp.dest('./www'));
+});
+
+gulp.task('git-check', function(done) {
+  if (!sh.which('git')) {
+    console.log(
+      '  ' + gutil.colors.red('Git is not installed.'),
+      '\n  Git, the version control system, is required to download Ionic.',
+      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+    );
+    process.exit(1);
+  }
+  done();
+});
