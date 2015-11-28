3260e7977ab787262eee6e0b8ebbf1612a4ae937
Alexander Swensen <alex.swensen@gmail.com>
2015-11-28 14:52:45 -0500

    Added leafletData, as we may end up needing that soon.

--- www/app/components/map/mapController.js
+++ www/app/components/map/mapController.js
@@ -1,4 +1,4 @@
-viewController.controller('mapController', function ($scope, $ionicModal, $timeout, gw2api) {
+viewController.controller('mapController', function ($scope, $ionicModal, $timeout, gw2api, leafletData) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
